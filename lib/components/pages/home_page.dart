2d8be866bc559b1b39add8272cf813b1abc304ec
snse-1205 <164691955+snse-1205@users.noreply.github.com>
2025-08-14 02:33:50 -0600

    fgyhujio

--- lib/components/pages/home_page.dart
+++ lib/components/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:uth_request_flutter_application/components/componentsAdmin/panelControl/views/panelControl.dart';
 import 'package:uth_request_flutter_application/components/notificaciones/views/notificacionesVista.dart';
 import 'package:uth_request_flutter_application/components/peticiones/views/createRequest.dart';
 import 'package:uth_request_flutter_application/components/shared/menu_drawer.dart';
